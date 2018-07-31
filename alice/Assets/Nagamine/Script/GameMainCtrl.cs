61589f8172c73e52bd2943e61de191c4579074e0
c17016 <c17016@std.it-college.ac.jp>
2018-07-31 11:17:10 +0900

    ブランチの作成、Unityに自分のファイルを追加

---
+++ alice/Assets/Nagamine/Script/GameMainCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMainCtrl : MonoBehaviour
+{     
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    public void ARcamera()
+    {
+        SceneManager.LoadScene("ARcamera");
+    }
+}
