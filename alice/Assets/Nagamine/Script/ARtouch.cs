e68d70514e1bc6af460172ace268e3b6048c4ac4
c17016 <c17016@std.it-college.ac.jp>
2018-08-02 17:14:13 +0900

    Vuforiaの追加とMainUIのベースを作成。

---
+++ alice/Assets/Nagamine/Script/ARtouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ARtouch : MonoBehaviour
+{
+    //GameObject gameObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                //Debug.Log(hit.gameObject.name);
+                if(hit.collider.tag == "Alice")
+                {
+                    print("rayがAliceに当たった。");
+                    //SceneManager.LoadScene("Q3");
+                }
+            }
+        }
+    }
+}
