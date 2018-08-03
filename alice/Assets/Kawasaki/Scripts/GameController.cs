d69935b215a89d29c2a36512f3d8232f8f323614
DaiyaKawasaki_c17006 <c17006@std.it-college.ac.jp>
2018-08-03 13:56:37 +0900

    シーンQ4を追加

---
+++ alice/Assets/Kawasaki/Scripts/GameController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour {
+
+    public bool clear = false;
+    public bool gameover = false;
+
+    public bool flag1 = true;
+    public bool flag4 = false;
+    public bool flag5 = false;
+    public bool flag10 = false;
+
+    public GameObject Playerobj;
+
+    float time = 10.0f;
+    Text timeText;
+
+    void Start() {
+        timeText = GameObject.Find("timeText").GetComponent<Text>();
+        Instantiate(Playerobj, Vector3.zero, Quaternion.identity);
+
+
+    }
+
+    void Update() {
+        if (time <= 0) {
+            GameOver();
+            return;
+        }
+
+        time -= Time.deltaTime;
+        timeText.text = "残り" + time.ToString("F0") + "秒";
+
+    }
+
+    public void Reset(GameObject destroy) {
+
+        //GameObject Player = GameObject.FindWithTag("Player");
+        Destroy(destroy);
+        Instantiate(Playerobj, Vector3.zero, Quaternion.identity);
+
+        flag1 = true;
+        flag4 = false;
+        flag5 = false;
+        flag10 = false;
+    }
+
+    public void Clear() {
+        //SceneManager.LoadScene("Clear");
+        timeText.text = "Clear";
+        clear = true;
+
+    }
+
+    void GameOver() {
+        //SceneManager.LoadScene("GameOver");
+        timeText.text = "GameOver";
+        gameover = true;
+
+
+    }
+}
