e7449df2d9e5b3c21952b40f91f206d7a711c592
c17016 <c17016@std.it-college.ac.jp>
2018-08-08 19:37:57 +0900

    一部ARマーカーを修正、素材の追加、スクリプトの修正、タイマーをとりあえず完成

--- alice/Assets/Nagamine/Script/GameMainCtrl.cs
+++ alice/Assets/Nagamine/Script/GameMainCtrl.cs
@@ -5,23 +5,47 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameMainCtrl : MonoBehaviour
-{
-    float countTime = 1200;
-    public Text timetext;
+{             
+    public Text timer;
+    Text timertext;
 
+    //bool card1;
+    //bool card4;
+    //bool card5;
+    //bool card10;
+    //bool Q2;
+    //bool Q3;
+    //bool Q4;
+    //bool Q5;
+    //bool Q6;
+    
     void Start ()
-    {
-        //timetext = GetComponent<Text>();
-		
-	}
-	
+    {                       
+        timertext = timer.GetComponent<Text>();
+    }   	
 	
 	void Update ()
     {
-        countTime -= Time.deltaTime; 
-        int minute = (int)countTime / 60;
-        int second = (int)countTime % 60;
-        timetext.GetComponent<Text>().text = minute.ToString("F0") + " : " + second.ToString("F0"); 
+        TimeDisplay();  
+        
+        // デバッグ用
+        if(Input.GetMouseButtonDown(0))  
+            TimeCtrl.f_count = true;         
+    }  
+    
+    void TimeDisplay()
+    {                 
+        int minute = (int)TimeCtrl.countTime / 60;
+        int second = (int)TimeCtrl.countTime % 60;
+
+        if (second < 10)
+            timertext.text = minute.ToString("F0") + " : 0" + second.ToString("F0");
+        else if (minute < 10)
+            timertext.text = "0" + minute.ToString("F0") + " : " + second.ToString("F0");
+        else if (second < 10 && minute < 10)
+            timertext.text = "0" + minute.ToString("F0") + " : 0" + second.ToString("F0");
+        else
+            timertext.text = minute.ToString("F0") + " : " + second.ToString("F0");   
     }
 
     public void ARcamera()
