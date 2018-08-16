d4a761c49f81ba3eb85546d37ebf8fcd167e9ae6
DaiyaKawasaki_c17006 <c17006@std.it-college.ac.jp>
2018-08-16 23:26:06 +0900

    最終調整

--- alice/Assets/Kawasaki/Scripts/Player.cs
+++ alice/Assets/Kawasaki/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField]
     float speed = 10.0f;
     GameController GC;
     Rigidbody rb;
@@ -49,19 +50,22 @@ public class Player : MonoBehaviour {
 
         if (other.gameObject.tag == "D_card1" && GC.flag1) {
             GC.CurrectSE();
+            GC.Currectimage_h1.SetActive(true);
             GC.flag1 = false;
             GC.flag4 = true;
         } else if (other.gameObject.tag == "D_card4" && GC.flag4) {
             GC.CurrectSE();
+            GC.Currectimage_d4.SetActive(true);
             GC.flag4 = false;
             GC.flag5 = true;
         } else if (other.gameObject.tag == "D_card5" && GC.flag5) {
             GC.CurrectSE();
-
+            GC.Currectimage_c5.SetActive(true);
             GC.flag5 = false;
             GC.flag10 = true;
         } else if (other.gameObject.tag == "D_card10" && GC.flag10) {
             GC.CurrectSE();
+            GC.Currectimage_s10.SetActive(true);
             GC.flag10 = false;
             GC.Clear();
         } else {
