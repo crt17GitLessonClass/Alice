8fe620c434aabbffc23d031b07daa3582df37a61
c17016 <c17016@std.it-college.ac.jp>
2018-08-16 22:38:06 +0900

    みやぐにのサウンドファイルに音追加、音入れ途中

--- alice/Assets/Nagamine/Script/TutorialCtrl.cs
+++ alice/Assets/Nagamine/Script/TutorialCtrl.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TutorialCtrl : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class TutorialCtrl : MonoBehaviour
+{
+    AudioSource SE;
+                   
+    void Start ()
+    {
+        SE = GetComponent<AudioSource>();
+    } 	
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
     public void ARcamera()
+    {
+        SE.PlayOneShot(SE.clip);
+        Invoke("ARcameraSceneLoad", 1.0f);        
+    }
+
+    void ARcameraSceneLoad()
     {
         SceneManager.LoadScene("ARcamera");
     }
